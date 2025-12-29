0922a1a390096434518fabc399f011f50d015318
irinoirino <genkishoot@icloud.com>
2025-12-29 21:07:02 +0900

    ヘッダーの装飾

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,18 +34,21 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <CustomUserButton />
-            </SignedIn>
+          <header className="flex justify-between items-center p-4 gap-4 h-16">
+            <div className="font-bold text-lg">Atcoder memo</div>
+            <div className="flex items-center gap-4">
+              <SignedOut>
+                <SignInButton />
+                <SignUpButton>
+                  <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </SignedOut>
+              <SignedIn>
+                <CustomUserButton />
+              </SignedIn>
+            </div>
           </header>
           {children}
         </body>
